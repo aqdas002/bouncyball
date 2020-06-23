bd26f65e54e94438fada21c70197011ae96d4c07
aqdas002 <42019890+aqdas002@users.noreply.github.com>
2020-06-23 23:22:54 +0530

    Updates

---
+++ precache-manifest.457223ac27b63ddaab2f8d219ba4c214.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b45f34888a91d6a1374bcdd3f88dd7",
+    "url": "/bouncyball/index.html"
+  },
+  {
+    "revision": "f6eca99cf1d4549a2132",
+    "url": "/bouncyball/static/css/main.ed96179d.chunk.css"
+  },
+  {
+    "revision": "18692cc531a6e41cc81d",
+    "url": "/bouncyball/static/js/2.a63800a4.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/bouncyball/static/js/2.a63800a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6eca99cf1d4549a2132",
+    "url": "/bouncyball/static/js/main.de18f2cb.chunk.js"
+  },
+  {
+    "revision": "e50ff9fad749a9c66f2e",
+    "url": "/bouncyball/static/js/runtime-main.399983da.js"
+  }
+]);
